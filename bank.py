9a3092f19d9ee30493a3952b70e338863c4a6970
Alejandro Pasalagua <alejandrop@vmware.com>
2024-07-16 13:39:36 -0600

    ANother test

--- bank.py
+++ bank.py
@@ -9,10 +9,12 @@ class Bank:
         self.exchange_rates = {}
 
     def add_exchange_rate(self, currency_from, currency_to, rate):
+        """This is a method."""
         key = f"{currency_from}->{currency_to}"
         self.exchange_rates[key] = rate
 
     def convert(self, a_money, a_currency):
+        """This is another method."""
         if a_money.currency == a_currency:
             return Money(a_money.amount, a_currency)
         key = f"{a_money.currency}->{a_currency}"
